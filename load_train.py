3484811443d373d7334bd920429ddc7f15aa0b01
shehabeldeen <shehabeldeen555@gmail.com>
2018-11-20 17:19:56 +0200

    load train

--- load_train.py
+++ load_train.py
@@ -3,59 +3,61 @@ import numpy as np
 import math
 
 
-def load_train_pclass_sex():
+def load_train(features):
     dataset = pd.read_csv("train.csv")
     m = len(dataset)
+    features_data = []
+    for i in range(len(features)):
+        features_data.append(load_train_feature(features[i]))
+
     x = np.array([np.ones(m, )], dtype=np.float32)
-    dataset['Pclass'] = (dataset['Pclass'] - dataset['Pclass'].mean()) / dataset['Pclass'].std()
-    p_class = np.array(dataset['Pclass'], dtype=np.float32)
-    sex = np.array(dataset['Sex'])
-    for i in range(len(sex)):
-        sex[i] = 1 if sex[i] == 'male' else 0
-    x = np.stack((x[0], p_class, sex))
+    x = np.stack((x[0]))
+    for i in range(len(features_data)):
+        x = np.stack(features_data[i])
+
     y = np.array(dataset['Survived'])
     y = y.reshape((891, 1))
     return x, y
 
 
-def load_train_sex_pclass_age():
-    dataset = pd.read_csv("train.csv")
-    m = len(dataset)
-    x = np.array([np.ones(m, )], dtype=np.float32)
-
-    ages = np.array(dataset['Age'])
-    for i in range(m):
-        if np.isnan(ages[i]):
-            ages[i] = np.nanmean(ages)
-    ages = (ages - ages.mean()) / ages.std()
-    sex = np.array(dataset['Sex'])
-    for i in range(len(sex)):
-        sex[i] = 1 if sex[i] == 'male' else 0
-    p_class = np.array(dataset['Pclass'], dtype=np.float32)
-    x = np.stack((x[0], p_class, sex, ages))
-    y = np.array(dataset['Survived'])
-    y = y.reshape((891, 1))
-    return x, y
+# def load_train_sex_pclass_age():
+#     dataset = pd.read_csv("train.csv")
+#     m = len(dataset)
+#     x = np.array([np.ones(m, )], dtype=np.float32)
+#
+#     ages = np.array(dataset['Age'])
+#     for i in range(m):
+#         if np.isnan(ages[i]):
+#             ages[i] = np.nanmean(ages)
+#     ages = (ages - ages.mean()) / ages.std()
+#     sex = np.array(dataset['Sex'])
+#     for i in range(len(sex)):
+#         sex[i] = 1 if sex[i] == 'male' else 0
+#     p_class = np.array(dataset['Pclass'], dtype=np.float32)
+#     x = np.stack((x[0], p_class, sex, ages))
+#     y = np.array(dataset['Survived'])
+#     y = y.reshape((891, 1))
+#     return x, y
 
 #load specific feature from the training dataset
-def load_train_feature(feature):
+def load_train_feature(feature_name):
     dataset = pd.read_csv("train.csv")
     m = len(dataset)
-    feature = np.array(dataset[feature], dtype=np.float32)
+    feature = np.array(dataset[feature_name])
 
     # if the feature is Sex
-    if (feature == "Sex"):
+    if (feature_name == "Sex"):
         for i in range(len(feature)):
             feature[i] = 1 if feature[i] == 'male' else 0
 
     # if  feature is Age
-    if (feature == "Age"):
+    if (feature_name == "Age"):
         for i in range(m):
             if np.isnan(feature[i]):
                 feature[i] = np.nanmean(feature)
 
     # if feature is Embarked
-    if (feature == "Embarked"):
+    if (feature_name == "Embarked"):
         for i in range(len(feature)):
             if (feature[i] == 'C'):
                 feature[i] = 1
@@ -65,7 +67,7 @@ def load_train_feature(feature):
                 feature[i] = 3
 
     # feature scaling
-    if (feature != "Sex"):
+    if (feature_name != "Sex"):
         feature = (feature - feature.mean()) / feature.std()
 
     return feature
--- problem.py
+++ problem.py
@@ -1,26 +1,27 @@
 
 import numpy as np
 from logistic_regression import logistic_regression, predict
-from load_train import load_train_pclass_sex
+from load_train import load_train
 from load_test import load_test_pclass_age
 from calculations import accuracy
 
 if __name__ == '__main__':
-    x, y = load_train_pclass_sex()
+    features = ["Pclass", "Sex"]
+    x, y = load_train(features)
     theta = np.zeros((x.shape[0], 1))
     theta = logistic_regression(x.transpose(), y, theta, 0.1, 500)
-    # print(theta)
-    x_test, y_test, ids = load_test_pclass_age()
+    print(x)
+    # x_test, y_test, ids = load_test_pclass_age()
     # print(ids)
-    y_predict = predict(x_test, theta)
+    # y_predict = predict(x_test, theta)
 
-    print(accuracy(y_predict, y_test))
-    f = open("ans.csv", 'w')
-    f.write("PassengerId,Survived\n")
-    for i in range(418):
-        f.write(str(ids[i]))
-        f.write(',')
-        f.write(str(y_predict[i][0]))
-        f.write('\n')
-    f.close()
+    # print(accuracy(y_predict, y_test))
+    # f = open("ans.csv", 'w')
+    # f.write("PassengerId,Survived\n")
+    # for i in range(418):
+    #     f.write(str(ids[i]))
+    #     f.write(',')
+    #     f.write(str(y_predict[i][0]))
+    #     f.write('\n')
+    # f.close()
 
